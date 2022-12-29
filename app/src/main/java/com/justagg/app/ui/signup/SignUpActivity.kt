11374c1c74975548dcd806ec121f11c14c00a61d
Talha Zahid <talha.zahid28@gmail.com>
2022-12-29 16:51:50 +0500

    SignUp UI

---
+++ app/src/main/java/com/justagg/app/ui/signup/SignUpActivity.kt
@@ -0,0 +1,17 @@
+package com.justagg.app.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.justagg.app.databinding.ActivitySignInBinding
+import com.justagg.app.databinding.ActivitySignUpBinding
+import com.justagg.app.util.viewBinding
+
+class SignUpActivity : AppCompatActivity() {
+
+    private val binding by viewBinding(ActivitySignUpBinding::inflate)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
