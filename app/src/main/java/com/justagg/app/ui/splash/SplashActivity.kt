026750ed347e58bcbe543f2c807b4d847044f994
Talha Zahid <talha.zahid28@gmail.com>
2022-12-29 16:45:34 +0500

    Added splash time 500ms

--- app/src/main/java/com/justagg/app/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/justagg/app/ui/splash/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.justagg.app.ui.splash
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.justagg.app.R
@@ -7,19 +8,43 @@ import com.justagg.app.databinding.ActivitySplashBinding
 import com.justagg.app.ui.intro.IntroActivity
 import com.justagg.app.util.loadImage
 import com.justagg.app.util.openActivity
+import com.justagg.app.util.snackBar
 import com.justagg.app.util.viewBinding
+import java.util.*
+import kotlin.concurrent.schedule
 
 class SplashActivity : AppCompatActivity() {
 
     private val binding by viewBinding(ActivitySplashBinding::inflate)
+    private var i = 0
+    private var timer = Timer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         loadImage(R.drawable.ic_logo, binding.ivLogo)
 
-        binding.ivLogo.setOnClickListener { openActivity(IntroActivity::class.java) }
+        timer.schedule(500) {
+            openActivity(IntroActivity::class.java)
+        }
 
+    }
+
+    override fun onBackPressed() {
+        if (i == 0) {
+            i++
+            binding.root.snackBar("Press back again to exit.")
+        } else {
+            val a = Intent(Intent.ACTION_MAIN)
+            a.addCategory(Intent.CATEGORY_HOME)
+            a.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(a)
+        }
+    }
+
+    override fun onPause() {
+        timer.cancel()
+        super.onPause()
     }
 
 }
